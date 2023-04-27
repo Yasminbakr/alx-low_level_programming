2117bcec96166f3368d1fe69551b60f181a28030
Yasminbakr <yasminbakr2019@gmail.com>
2023-04-27 16:08:52 -0700

    g

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints diagonal line n times.
+ * @n: times diagonal line is printed.
+ * Return: no return.
+ */
+
+void print_diagonal(int n)
+{
+
