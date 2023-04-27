f3a65f20731c078cc198e17942bab2bb6e2d3d18
Yasminbakr <yasminbakr2019@gmail.com>
2023-04-27 14:41:08 -0700

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
