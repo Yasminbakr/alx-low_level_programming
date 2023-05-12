193ad6e9b5e27b3fa1f5b265c0f5c7338a32a88e
Yasminbakr <yasminbakr2019@gmail.com>
2023-05-11 17:14:02 -0700

    f

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen_recursion - check the code for Holberton School students.
- * @s: Inputting string
- * Return: Always 0.
+ * @s: Inputting string(pointer)
+ * Return: the number of string length
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 		return (0);
 
-	return (_strlen_recursion(s + 1) + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
