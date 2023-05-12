f5fe88eab4c453fbf5beef230eb4c442b122ac5b
Yasminbakr <yasminbakr2019@gmail.com>
2023-05-11 19:56:09 -0700

    d

--- 0x08-recursion/..6-is_prime_number.c
+++ 0x08-recursion/..6-is_prime_number.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
-
 int check_prime(int n, int i);
-
 /**
   * is_prime_number - Returns if a number is prime
   * @n: the number to be checked
+  * @i: iteration number
   *
   * Return: integer value
   */
+
 int is_prime_number(int n)
 {
-	return (check_prime(n, 1));
+	return (check_prime(n, 2));
 }
 
 /**
@@ -26,12 +26,13 @@ int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
-
-	if (n % i == 0 && i > 1)
-		return (0);
-
-	if ((n / i) < i)
+	else if (n == 2)
 		return (1);
-
-	return (check_prime(n, i + 1));
+	else if (n % i == 0)
+		return (0);
+	else if ((n / i) < i)
+		return (1);
+	else
+		return (check_prime(n, i + 1));
 }
+
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 
+int check_prime(int n, int i);
+
 /**
   * is_prime_number - Returns if a number is prime
   * @n: the number to be checked
-  * @i: iteration number
   *
   * Return: integer value
   */
-
-int check_prime(int n, int i);
 int is_prime_number(int n)
 {
-	return (check_prime(n, 2));
+	return (check_prime(n, 1));
 }
 
 /**
@@ -27,11 +26,12 @@ int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
-	else if (n == 2)
-		return (1);
-	else if (n % i == 0)
-		return (0);
-	else
-		return (check_prime(n, i + 1));
-}
 
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
+		return (1);
+
+	return (check_prime(n, i + 1));
+}
