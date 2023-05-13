7a342ec579ed45edb96f0b67b93fab1519460907
Yasminbakr <yasminbakr2019@gmail.com>
2023-05-12 17:58:44 -0700

    understood

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: count of arguments
+ * @argv: argument vector array of strings that hold argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
