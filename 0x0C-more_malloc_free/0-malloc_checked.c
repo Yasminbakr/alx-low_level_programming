1c448a123fbe7a535d94694c029924d9f6fdd6bb
Yasminbakr <yasminbakr2019@gmail.com>
2023-05-18 16:31:55 -0700

    f

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  * Return: a pointer to the allocated memory
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *mem;
+	int *mem;
 
 	mem = malloc(b);
 
 	if (mem == NULL)
 		exit(98);
+
 
 	return (mem);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
-int main(int argc ,char *argv[]);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
